05192c7a8c32ca57c232781a4df408c5650a723e
Christopher Finch <christopher.h.finch@gmail.com>
2018-02-02 06:22:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,6 +3,8 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
+  valid_move?(board,index)
+  
 
 #  display_board(board)
 
@@ -36,7 +38,7 @@ end
 #code your #valid_move? method here
 def valid_move?(board,index)
   if index.between?(0,8) && position_taken?(board,index)
-    true
+    puts "Turru"    
   end
 end
 
